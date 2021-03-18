cd750d6df6abd6bedcb1884f6f5d0eb7a21f17bb
humayun-ahmad <humayunraj789@gmail.com>
2021-03-18 17:58:16 +0600

    z-index is very important

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000');
+    
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        }else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '25%'
     });
+    
+       /*
+   var waypoints = $('#handler-first').waypoint(function(direction) {
+      notify(this.element.id + ' hit 25% from top of window') 
+    }, {
+      offset: '25%'
+    })
+    */
+    
 });
