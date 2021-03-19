05014e723eb463611bb04a2ab82237fd379aecf3
humayun-ahmad <humayunraj789@gmail.com>
2021-03-19 07:58:02 +0600

    scroll animation add

--- omnifood/resources/js/script.js
+++ omnifood/resources/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     
+    /* sticky navbar part start */
+    
     $('.js--section-features').waypoint(function(direction) {
         if(direction == "down") {
             $('nav').addClass('sticky');
@@ -9,13 +11,27 @@ $(document).ready(function() {
     }, {
       offset: '25%'
     });
+    /* sticky navbar part end */
     
-       /*
-   var waypoints = $('#handler-first').waypoint(function(direction) {
-      notify(this.element.id + ' hit 25% from top of window') 
-    }, {
-      offset: '25%'
-    })
-    */
+    
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
+    });
     
 });
+
+
+
+
+
+
+
+
+
+
